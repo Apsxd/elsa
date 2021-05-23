4098cf3b13fa9ce2eb73812a0878f6c5825b9aee
Lucido <74089102+Basi-mon@users.noreply.github.com>
2021-05-23 13:22:50 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -130,8 +130,7 @@ def start(bot: Bot, update: Update, args: List[str]):
             update.effective_message.reply_photo(
                 TECHNO_IMG,
                 PM_START_TEXT.format(escape_markdown(first_name), escape_markdown(bot.first_name), OWNER_ID),
-                parse_mode=ParseMode.MARKDOWN, reply_markup=InlineKeyboardMarkup(
-                       [
+                parse_mode=ParseMode.MARKDOWN, reply_markup=InlineKeyboardMarkup([
                         [
                             InlineKeyboardButton(
                                 text="☑️ Add me",
@@ -169,6 +168,7 @@ def start(bot: Bot, update: Update, args: List[str]):
                     ],
                 ),
             )
+                       
     else:
         update.effective_message.reply_text("Yuss, I am Already ONline")
 
